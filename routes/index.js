bee1ba0d2ab0b84029e87e0be8ff4ba3ca17578b
Ben Hall <ben@benhall.me.uk>
2016-06-18 22:38:31 +0100

    Add a /machine endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/machine', function(req, res, next) {
+  var os = require("os");
+  var host = os.hostname();
+
+  res.end('Request processed by ' + host);
+});
+
 module.exports = router;
